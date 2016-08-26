d58cd3c7a97d78a7f909b6ee1a48e2d6f0b1e543
MWessel27 <MWessel27@gmail.com>
2016-08-25 23:48:08 -0400

    Updated to a truly random secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 # default config
 class BaseConfig(object):
     DEBUG = False
-    SECRET_KEY = 'my precious'
+    SECRET_KEY = '\xa1\xc74jhw\xce\x88]\xf42`\xdbX\x0c\x1d\x8en_\x8b\x03\xf2GJ'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
